494f801f0fd4801b391ca9735d06e75f38afe7a2
gksruf2848 <4455qqq@naver.com>
2022-09-28 16:07:15 +0900

    .

--- 125_valid_palindrome.py
+++ 125_valid_palindrome.py
@@ -1,4 +1,13 @@
-strs = []
-for char in s:
-    if char.isalnum():
-        strs.append(char.lower())
+class Solution:
+    def isPalindrome(self, s: str) -> bool:   
+        strs = []
+        for char in s:
+            if char.isalnum():
+                str.append(char.lower())
+
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+
+        return True
+        
