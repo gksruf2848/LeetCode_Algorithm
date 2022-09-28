b33206535ca4906c1578bf76b3b1b4decffa4203
gksruf2848 <4455qqq@naver.com>
2022-09-28 17:04:34 +0900

    .

--- 125_valid_palindrome.py
+++ 125_valid_palindrome.py
@@ -1,3 +1,6 @@
+import collections
+import re
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:   
         strs = []
@@ -11,3 +14,27 @@ class Solution:
 
         return True
         
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #자료형 데크로 선언
+        strs: Deque = collections.deque()
+
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+    
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+        
+        return True
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        #정규식으로 불필요한 문자 필터링
+        s = re.sub('[^a-z0-9]', '', s)
+
+        return s == s[::-1] #슬라이싱
